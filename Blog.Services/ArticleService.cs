9c7aa2558b33d30e132d2742d91383a33c3a1f54
MilenDinev <dinev.milen@gmail.com>
2023-10-22 13:58:37 +0300

    Implementing Crud methods

--- Blog.Services/ArticleService.cs
+++ Blog.Services/ArticleService.cs
@@ -4,78 +4,111 @@
     using Data;
     using Data.Entities;
     using Data.Models.RequestModels.Article;
-    using Data.Models.ResponseModels.Article;
+    using Base;
     using Constants;
-    using Repository;
     using Interfaces;
     using Handlers.Exceptions;
     using Repository.Interfaces;
 
-    public class ArticleService : RepositoryService<Article>, IArticleService
+    public class ArticleService : PublicationBaseService,  IArticleService
     {
-        private readonly IFinder finder;
-        private readonly IMapper mapper;
+        private readonly IMapper mapper;    
 
-        public ArticleService(ApplicationDbContext dbContext,
+        public ArticleService(
+            IMapper mapper,
             IFinder finder,
-            IMapper mapper) : base(dbContext)
+            ApplicationDbContext dbContext
+            ) 
+            : base(finder, dbContext)
         {
-            this.finder = finder;
             this.mapper = mapper;
         }
 
-        public async Task<CreatedArticleModel> CreateAsync(ArticleCreateModel articleModel, string userId)
+        public async Task CreateAsync(ArticleCreateModel articleModel, string userId)
         {
             await this.ValidateCreateInputAsync(articleModel);
 
             var article = mapper.Map<Article>(articleModel);
 
             await CreateEntityAsync(article, userId);
-
-            return mapper.Map<CreatedArticleModel>(article);
         }
 
-        public async Task<EditedArticleModel> EditAsync(ArticleEditModel articleModel, string articleId, string modifierId)
+        public async Task EditAsync(ArticleEditModel articleModel, string articleId, string modifierId)
         {
-            var article = await this.finder.FindByIdOrDefaultAsync<Article>(articleId);
+            var article = await this.GetByIdAsync(articleId);
 
             article.Title = articleModel.Title ?? article.Title;
-
+            article.Description = articleModel.Description ?? article.Description;
+            article.Content = articleModel.Content ?? article.Content;
+            article.ImageUrl = articleModel.ImageUrl ?? article.ImageUrl;
+            article.VideoUrl = articleModel.VideoUrl ?? article.VideoUrl;
+            article.ExternalArticleUrl = articleModel.ExternalArticleUrl ?? article.ExternalArticleUrl;
+            article.TopPick = articleModel.TopPick ?? article.TopPick;
+            article.SpecialOffer = articleModel.TopPick ?? article.SpecialOffer;
+                      
             await SaveModificationAsync(article, modifierId);
-
-            return mapper.Map<EditedArticleModel>(article);
         }
 
-        public async Task<DeletedArticleModel> DeleteAsync(string articleId, string modifierId)
+        public async Task DeleteAsync(string articleId, string modifierId)
         {
-            var article = await this.finder.FindByIdOrDefaultAsync<Article>(articleId);
+            var article = await this.GetByIdAsync(articleId);
 
             await DeleteEntityAsync(article, modifierId);
-
-            return mapper.Map<DeletedArticleModel>(article);
         }
 
-        public async Task<ArticleListModel> GetByIdAsync(string articleId)
+        public async Task UpVote(int vote, string articleId, string modifierId)
         {
-            var article = await this.finder.FindByIdOrDefaultAsync<Article>(articleId);
+            var article = await this.finder.GetByIdAsync<Article>(articleId);
 
-            return mapper.Map<ArticleListModel>(article);
+            var downVote = article.DownVotes.FirstOrDefault(x => x.UserId == modifierId);
+
+            if (downVote != null)
+            {
+                this.dbContext.Remove(downVote);
+
+            }
+
+            var upVote = new UpVote
+            {
+                Id = Guid.NewGuid().ToString(),
+                ArticleId = article.Id,
+                UserId = modifierId
+            };
+
+            await this.dbContext.AddAsync(upVote);
+            await SaveModificationAsync(article, modifierId);
+        }
+
+        public async Task DownVote(int vote, string articleId, string modifierId)
+        {
+            var article = await this.finder.GetByIdAsync<Article>(articleId);
+
+            var upVote = article.UpVotes.FirstOrDefault(x => x.UserId == modifierId);
+
+            if (upVote != null)
+            {
+                this.dbContext.Remove(upVote);
+                
+            }
+
+            var downVote = new DownVote
+            {
+                Id = Guid.NewGuid().ToString(),
+                ArticleId = article.Id,
+                UserId = modifierId
+            };
+
+            await this.dbContext.AddAsync(downVote);
+            await SaveModificationAsync(article, modifierId);
         }
 
         private async Task ValidateCreateInputAsync(ArticleCreateModel articleModel)
         {
-            var isAnyArticle = await this.finder.AnyByStringAsync<Article>(articleModel.Title);
+            var isAnyArticle = await this.AnyByTitleAsync(articleModel.Title);
             if (isAnyArticle)
                 throw new ResourceAlreadyExistsException(string.Format(
                     ErrorMessages.EntityAlreadyExists,
                     typeof(Article).Name, articleModel.Title));
-        }
-
-        public async Task<ICollection<ArticleListModel>> GetAllAsync()
-        {
-            var articles = await this.finder.GetAllAsync<Article>();
-
-            return mapper.Map<ICollection<ArticleListModel>>(articles);
         }
     }
 }
