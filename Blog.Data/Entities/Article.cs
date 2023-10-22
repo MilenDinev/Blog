e07478010dfa0b2bb0daf1726691da84876b33fa
MilenDinev <dinev.milen@gmail.com>
2023-10-22 13:55:25 +0300

    Implementing UpVotes and DownVotes

--- Blog.Data/Entities/Article.cs
+++ Blog.Data/Entities/Article.cs
@@ -9,14 +9,14 @@
             this.FavoriteByUsers = new HashSet<User>();
             this.Tags= new HashSet<Tag>();
             this.PricingStrategies = new HashSet<PricingStrategy>();
+            this.UpVotes = new HashSet<UpVote>();
+            this.DownVotes = new HashSet<DownVote>();
         }
 
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Title { get; set; }
         public string Description { get; set; }
         public string Content { get; set; }
-        public int UpVotes { get; set; }
-        public int DownVotes { get; set; }
         public string? ImageUrl { get; set; }
         public string? VideoUrl { get; set; }
         public string? ExternalArticleUrl { get; set; }
@@ -30,6 +30,8 @@
         public virtual User LastModifier { get; set; }
         public bool Deleted { get; set; }
         public DateTime LastModifiedOn { get; set; }
+        public virtual ICollection<UpVote> UpVotes { get; set; }
+        public virtual ICollection<DownVote> DownVotes { get; set; }
         public virtual ICollection<Tag> Tags { get; set; }
         public virtual ICollection<User> FavoriteByUsers { get; set; }
         public virtual ICollection<User> LikedByUsers { get; set; }
--- Blog.Data/Entities/User.cs
+++ Blog.Data/Entities/User.cs
@@ -8,18 +8,21 @@
     {
         public User()
         {
-            CreatedArticles = new HashSet<Article>();
-            ModifiedArticles = new HashSet<Article>();
-            FavoriteArticles = new HashSet<Article>();
-            LikedArticles = new HashSet<Article>();
-            CreatedTags = new HashSet<Tag>();
-            ModifiedTags = new HashSet<Tag>();
-            CreatedPricingStrategies = new HashSet<PricingStrategy>();
-            ModifiedPricingStrategies = new HashSet<PricingStrategy>();
+            this.CreatedArticles = new HashSet<Article>();
+            this.ModifiedArticles = new HashSet<Article>();
+            this.FavoriteArticles = new HashSet<Article>();
+            this.LikedArticles = new HashSet<Article>();
+            this.CreatedTags = new HashSet<Tag>();
+            this.ModifiedTags = new HashSet<Tag>();
+            this.CreatedPricingStrategies = new HashSet<PricingStrategy>();
+            this.ModifiedPricingStrategies = new HashSet<PricingStrategy>();
+            this.UpVotes = new HashSet<UpVote>();
+            this.DownVotes = new HashSet<DownVote>();
         }
 
         public DateTime CreationDate { get; set; }
         public DateTime LastModifiedOn { get; set; }
+        public bool Deleted { get; set; }
         public virtual ICollection<Article> CreatedArticles { get; set; }
         public virtual ICollection<Article> ModifiedArticles { get; set; }
         public virtual ICollection<PricingStrategy> CreatedPricingStrategies { get; set; }
@@ -28,6 +31,7 @@
         public virtual ICollection<Article> LikedArticles { get; set; }
         public virtual ICollection<Tag> CreatedTags { get; set; }
         public virtual ICollection<Tag> ModifiedTags{ get; set; }
-        public bool Deleted { get; set; }
+        public virtual ICollection<UpVote> UpVotes { get; set; }
+        public virtual ICollection<DownVote> DownVotes { get; set; }
     }
 }
