70dfbba8e86d6584a6644694c845ea19cb29be4f
MilenDinev <dinev.milen@gmail.com>
2023-10-22 13:57:00 +0300

    Deleting

--- Blog.Data.Models/ResponseModels/Article/CreatedArticleModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Blog.Data.Models.ResponseModels.Article
-{
-    public class CreatedArticleModel
-    {
-        public string Id { get; set; }
-        public string Title { get; set; }
-    }
-}
--- Blog.Data.Models/ResponseModels/Article/DeletedArticleModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Blog.Data.Models.ResponseModels.Article
-{
-    public class DeletedArticleModel
-    {
-        public string Id { get; set; }
-        public string Title { get; set; }
-    }
-}
