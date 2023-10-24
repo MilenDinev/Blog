ef5071d5cb84025a0fe628e1b527cb12aa7c0353
MilenDinev <dinev.milen@gmail.com>
2023-10-24 03:35:13 +0300

    Initial Create

---
+++ Blog.Services/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task AddReviewToFavorite(string userId, string reviewId);
+
+        Task RemoveReviewFromFavorites(string userId, string reviewId);
+
+        Task VoteAsync(bool type, string reviewId, string userId);
+
+        Task ViewReviewAsync(string reviewId, string userId);
+
+        Task ViewVideoAsync(string videoId, string userId);
+
+        Task ViewArticleAsync(string articleId, string userId);
+    }
+}
