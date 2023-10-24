b5df431312c1ced2a86f83943c8c34ebbb019c29
MilenDinev <dinev.milen@gmail.com>
2023-10-24 03:36:23 +0300

    Refactoring

--- Blog.Services/ArticleService.cs
+++ Blog.Services/ArticleService.cs
@@ -1,25 +1,25 @@
 ﻿namespace Blog.Services
 {
+    using Microsoft.EntityFrameworkCore;
     using AutoMapper;
     using Data;
     using Data.Entities;
+    using Data.Models.ViewModels.Article;
     using Data.Models.RequestModels.Article;
-    using Base;
     using Constants;
     using Interfaces;
+    using Repository;
     using Handlers.Exceptions;
-    using Repository.Interfaces;
 
-    public class ArticleService : PublicationBaseService,  IArticleService
+    public class ArticleService : Repository<Article>, IArticleService
     {
-        private readonly IMapper mapper;    
+        private readonly IMapper mapper;
 
         public ArticleService(
             IMapper mapper,
-            IFinder finder,
             ApplicationDbContext dbContext
-            ) 
-            : base(finder, dbContext)
+            )
+            : base(dbContext)
         {
             this.mapper = mapper;
         }
@@ -38,14 +38,10 @@
             var article = await this.GetByIdAsync(articleId);
 
             article.Title = articleModel.Title ?? article.Title;
-            article.Description = articleModel.Description ?? article.Description;
-            article.Content = articleModel.Content ?? article.Content;
+            article.ProviderName = articleModel.ProviderName ?? article.ProviderName;
+            article.Url = articleModel.Url ?? article.Url;
             article.ImageUrl = articleModel.ImageUrl ?? article.ImageUrl;
-            article.VideoUrl = articleModel.VideoUrl ?? article.VideoUrl;
-            article.ExternalArticleUrl = articleModel.ExternalArticleUrl ?? article.ExternalArticleUrl;
-            article.TopPick = articleModel.TopPick ?? article.TopPick;
-            article.SpecialOffer = articleModel.TopPick ?? article.SpecialOffer;
-                      
+
             await SaveModificationAsync(article, modifierId);
         }
 
@@ -56,59 +52,78 @@
             await DeleteEntityAsync(article, modifierId);
         }
 
-        public async Task UpVote(int vote, string articleId, string modifierId)
-        {
-            var article = await this.finder.GetByIdAsync<Article>(articleId);
-
-            var downVote = article.DownVotes.FirstOrDefault(x => x.UserId == modifierId);
-
-            if (downVote != null)
-            {
-                this.dbContext.Remove(downVote);
-
-            }
-
-            var upVote = new UpVote
-            {
-                Id = Guid.NewGuid().ToString(),
-                ArticleId = article.Id,
-                UserId = modifierId
-            };
-
-            await this.dbContext.AddAsync(upVote);
-            await SaveModificationAsync(article, modifierId);
-        }
-
-        public async Task DownVote(int vote, string articleId, string modifierId)
-        {
-            var article = await this.finder.GetByIdAsync<Article>(articleId);
-
-            var upVote = article.UpVotes.FirstOrDefault(x => x.UserId == modifierId);
-
-            if (upVote != null)
-            {
-                this.dbContext.Remove(upVote);
-                
-            }
-
-            var downVote = new DownVote
-            {
-                Id = Guid.NewGuid().ToString(),
-                ArticleId = article.Id,
-                UserId = modifierId
-            };
-
-            await this.dbContext.AddAsync(downVote);
-            await SaveModificationAsync(article, modifierId);
-        }
-
         private async Task ValidateCreateInputAsync(ArticleCreateModel articleModel)
         {
-            var isAnyArticle = await this.AnyByTitleAsync(articleModel.Title);
+            var isAnyArticle = await this.AnyByStringAsync(articleModel.Url);
             if (isAnyArticle)
                 throw new ResourceAlreadyExistsException(string.Format(
                     ErrorMessages.EntityAlreadyExists,
-                    typeof(Article).Name, articleModel.Title));
+                    typeof(Article).Name, articleModel.Url));
+        }
+
+        public async Task<ICollection<ArticlePreviewModel>> GetArticlePreviewModelBundleAsync()
+        {
+            var articlePreviewModelBundle = await this.dbContext.Articles
+                .AsNoTracking()
+                .Where(x => !x.Deleted)
+                .Select(x => new ArticlePreviewModel
+                {
+                    Id= x.Id,
+                    Title = x.Title,
+                    ImageUrl = x.ImageUrl,
+                    ProviderName = x.ProviderName,
+                    UploadDate = x.CreationDate.ToString("dd/MM/yyyy"),
+                    Url = x.Url
+                }).ToListAsync();
+
+            return articlePreviewModelBundle;
+        }
+
+        public async Task<ArticleEditViewModel> GetArticleEditViewModelByIdAsync(string id)
+        {
+            var isAny = await this.AnyByIdAsync(id);
+
+            if (!isAny)
+                throw new ResourceNotFoundException(string.Format(
+                    ErrorMessages.EntityDoesNotExist, typeof(Article).Name));
+
+            var articleEditViewModel = await this.dbContext.Articles
+                .AsNoTracking()
+                .Where(x => x.Id == id && !x.Deleted)
+                .Select(x => new ArticleEditViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    ImageUrl = x.ImageUrl,
+                    ProviderName = x.ProviderName,
+                    Url = x.Url
+                }).SingleOrDefaultAsync();
+
+            return articleEditViewModel;
+
+        }
+
+        public async Task<ArticleDeleteViewModel> GetArticleDeleteViewModelByIdAsync(string id)
+        {
+            var isAny = await this.AnyByIdAsync(id);
+
+            if (!isAny)
+                throw new ResourceNotFoundException(string.Format(
+                    ErrorMessages.EntityDoesNotExist, typeof(Article).Name));
+
+            var articleDeleteViewModel = await this.dbContext.Articles
+            .AsNoTracking()
+            .Where(x => x.Id == id && !x.Deleted)
+            .Select(x => new ArticleDeleteViewModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                ProviderName = x.ProviderName,
+                UploadDate = x.CreationDate.ToString("dd/MM/yyyy"),
+                Url = x.Url
+            }).SingleOrDefaultAsync();
+
+            return articleDeleteViewModel;
         }
     }
 }
