efd398168106482e992e95a344d0a677f9563a1a
MilenDinev <dinev.milen@gmail.com>
2023-10-24 03:35:40 +0300

    Add methods

--- Blog.Services/Interfaces/IArticleService.cs
+++ Blog.Services/Interfaces/IArticleService.cs
@@ -1,17 +1,17 @@
 ﻿namespace Blog.Services.Interfaces
 {
-    using System.Threading.Tasks;
+    using Blog.Data.Models.ViewModels.Article;
+    using Blog.Data.Models.ViewModels.Video;
     using Data.Models.RequestModels.Article;
-    using Data.Models.ResponseModels.Article;
 
     public interface IArticleService
     {
         Task CreateAsync(ArticleCreateModel articleModel, string userId);
         Task EditAsync(ArticleEditModel articleModel, string articleId, string modifierId);
         Task DeleteAsync(string articleId, string modifierId);
-        Task<bool> AnyByIdAsync(string id);
-        Task<ArticlePreviewModel> GetArticlePreviewModelByIdAsync(string tag);
-        Task<ArticleCompleteModel> GetArticleCompleteModelByIdAsync(string tag);
         Task<ICollection<ArticlePreviewModel>> GetArticlePreviewModelBundleAsync();
+        Task<ArticleEditViewModel> GetArticleEditViewModelByIdAsync(string id);
+        Task<ArticleDeleteViewModel> GetArticleDeleteViewModelByIdAsync(string id);
+        Task<bool> AnyByIdAsync(string id);
     }
 }
