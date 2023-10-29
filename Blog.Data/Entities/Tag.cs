2109ab1b59b2e3ee79fc356a50d0a206df5caaed
MilenDinev <dinev.milen@gmail.com>
2023-10-29 03:03:17 +0200

    Adding Reviews Videos navigation properties

--- Blog.Data/Entities/Tag.cs
+++ Blog.Data/Entities/Tag.cs
@@ -6,7 +6,9 @@
     {
         public Tag()
         {
+            this.Reviews = new HashSet<Review>();
             this.Articles = new HashSet<Article>();
+            this.Videos = new HashSet<Video>();
         }
 
         public string Id { get; set; } = Guid.NewGuid().ToString();
@@ -19,6 +21,8 @@
         public virtual User LastModifier { get; set; }
         public DateTime LastModifiedOn { get; set; }
         public bool Deleted { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
         public virtual ICollection<Article> Articles { get; set; }
+        public virtual ICollection<Video> Videos { get; set; }
     }
 }
