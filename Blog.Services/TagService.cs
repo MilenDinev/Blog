f15c546f5719e4a251edd15a62fb9a6d53964050
MilenDinev <dinev.milen@gmail.com>
2023-10-29 03:10:44 +0200

    implementing GetTagByIdAsync

--- Blog.Services/TagService.cs
+++ Blog.Services/TagService.cs
@@ -11,7 +11,7 @@
 
     public class TagService : Repository<Tag>, ITagService
     {
-        private readonly IMapper mapper;
+        private readonly IMapper _mapper;
 
         public TagService(
             IMapper mapper,
@@ -19,21 +19,21 @@
             )
             : base(dbContext)
         {
-            this.mapper = mapper;
+            _mapper = mapper;
         }
 
         public async Task CreateAsync(TagCreateModel tagModel, string userId)
         {
-            await this.ValidateCreateInputAsync(tagModel);
+            await ValidateCreateInputAsync(tagModel);
 
-            var tag = mapper.Map<Tag>(tagModel);
+            var tag = _mapper.Map<Tag>(tagModel);
 
             await CreateEntityAsync(tag, userId);
         }
 
         public async Task EditAsync(TagEditModel tagModel, string tagId, string modifierId)
         {
-            var tag = await this.GetByIdAsync(tagId);
+            var tag = await GetByIdAsync(tagId);
 
             tag.Value = tagModel.Value ?? tag.Value;
 
@@ -42,14 +42,21 @@
 
         public async Task DeleteAsync(string tagId, string modifierId)
         {
-            var tag = await this.GetByIdAsync(tagId);
+            var tag = await GetByIdAsync(tagId);
 
             await DeleteEntityAsync(tag, modifierId);
         }
 
+        public async Task<Tag> GetTagByIdAsync(string tagId)
+        {
+            var tag = await GetByIdAsync(tagId);
+
+            return tag;
+        }
+
         private async Task ValidateCreateInputAsync(TagCreateModel tagModel)
         {
-            var isAnyTag = await this.AnyByStringAsync(tagModel.Value);
+            var isAnyTag = await AnyByStringAsync(tagModel.Value);
             if (isAnyTag)
                 throw new ResourceAlreadyExistsException(string.Format(
                     ErrorMessages.EntityAlreadyExists,
