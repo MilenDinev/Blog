82199042b0ee8a711a8b52ccb546e3b2a0c4bdee
MilenDinev <dinev.milen@gmail.com>
2023-10-29 03:08:34 +0200

    Adding error messages constants

--- Blog.Services/Constants/ErrorMessages.cs
+++ Blog.Services/Constants/ErrorMessages.cs
@@ -4,5 +4,7 @@
     {
         internal const string EntityDoesNotExist = @"{0} does not exist!";
         internal const string EntityAlreadyExists = @"{0} - '{1}' already exists!";
+        internal const string TagAlreadyAssigned = @"{0} - '{1}' is already assigned";
+        internal const string TagNotAssigned = @"{0} - '{1}' is not assigned!";
     }
 }
