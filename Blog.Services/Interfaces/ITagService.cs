d531410fb563017f8cf6a99e2bfad19e8a3d4dff
MilenDinev <dinev.milen@gmail.com>
2023-10-29 03:09:14 +0200

    Introducing GetTagByIdAsync method

--- Blog.Services/Interfaces/ITagService.cs
+++ Blog.Services/Interfaces/ITagService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Blog.Services.Interfaces
 {
+    using Data.Entities;
     using Data.Models.RequestModels.Tag;
 
     public interface ITagService
@@ -7,6 +8,7 @@
         Task CreateAsync(TagCreateModel tagModel, string userId);
         Task EditAsync(TagEditModel tagModel, string tagId, string modifierId);
         Task DeleteAsync(string tagId, string modifierId);
+        Task<Tag> GetTagByIdAsync(string tagId);
         Task<bool> AnyByIdAsync(string id);
     }
 }
