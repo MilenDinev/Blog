fb7b10acd537dcf9c36eb301212896f6bbf9a52a
MilenDinev <dinev.milen@gmail.com>
2023-10-29 03:08:09 +0200

    Updating mapping

--- Blog.Services/AutoMapperProfiles/ReviewMappingProfile.cs
+++ Blog.Services/AutoMapperProfiles/ReviewMappingProfile.cs
@@ -2,6 +2,7 @@
 {
     using AutoMapper;
     using Data.Entities;
+    using Data.Models.ViewModels.Review;
     using Data.Models.RequestModels.Review;
 
     public class ReviewMappingProfile : Profile
@@ -11,6 +12,11 @@
             CreateMap<ReviewCreateModel, Review>()
                 .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.Title.ToUpper()))
                 .ForMember(e => e.Tags, m => m.Ignore());
+            CreateMap<Review, ReviewPreviewModel>()
+                .ForMember(m => m.UpVotes, e => e.MapFrom(e => e.Votes.Count(x => x.Type == true && !x.Deleted)))
+                .ForMember(m => m.CreationDate, e => e.MapFrom(e => e.CreationDate.ToString("dd MMMM hh:mm tt")))
+                .ForMember(m => m.Tags, e => e.MapFrom(e => e.Tags.Select(t => t.Value).ToList()));
+
         }
     }
 }
