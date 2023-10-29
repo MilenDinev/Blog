c4344101ed95f87247901986807d933594bf9757
MilenDinev <dinev.milen@gmail.com>
2023-10-29 03:04:37 +0200

    Adding validation properties constants

--- Blog.Data.Models/Constants/AttributesParams.cs
+++ Blog.Data.Models/Constants/AttributesParams.cs
@@ -6,9 +6,20 @@
         internal const int TitleMaxLength = 100;
 
         internal const int DescriptionMinLength = 20;
-        internal const int DescriptionMaxLength = 300;
+        internal const int DescriptionMaxLength = 200;
 
-        internal const int ContentMinLength = 100;
+        internal const int ContentMinLength = 200;
 
+        internal const int ProviderMinLength = 3;
+        internal const int ProviderMaxLength = 50;
+
+        internal const int StrategyMinLength = 3;
+        internal const int StrategyMaxLength = 20;
+
+        internal const int TagValueMinLength = 3;
+        internal const int TagValueMaxLength = 10;
+
+        internal const int RelatedToolNameMinLength = 3;
+        internal const int RelatedToolNameMaxLength = 30;
     }
 }
