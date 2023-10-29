1a73c51e11d1f5b12dd0b681d7615c794a567575
MilenDinev <dinev.milen@gmail.com>
2023-10-29 03:11:35 +0200

    Implementing AssignTagAsync and refactoring other methods

--- Blog.Services/VideoService.cs
+++ Blog.Services/VideoService.cs
@@ -13,62 +13,98 @@
 
     public class VideoService : Repository<Video>, IVideoService
     {
-        private readonly IMapper mapper;
+        private readonly ITagService _tagService;
+        private readonly IMapper _mapper;
 
         public VideoService(
+            ITagService tagService,
             IMapper mapper,
             ApplicationDbContext dbContext
             )
             : base(dbContext)
         {
-            this.mapper = mapper;
+            _tagService = tagService;
+            _mapper = mapper;
         }
 
         public async Task CreateAsync(VideoCreateModel videoModel, string userId)
         {
-            await this.ValidateCreateInputAsync(videoModel);
+            await ValidateCreateInputAsync(videoModel);
 
-            var video = mapper.Map<Video>(videoModel);
+            var video = _mapper.Map<Video>(videoModel);
 
             await CreateEntityAsync(video, userId);
         }
 
         public async Task EditAsync(VideoEditModel videoModel, string videoId, string modifierId)
         {
-            var video = await this.GetByIdAsync(videoId);
+            var video = await GetByIdAsync(videoId);
 
             video.Title = videoModel.Title ?? video.Title;
             video.Url = videoModel.Url ?? video.Url;
             video.ImageUrl = videoModel.ImageUrl ?? video.ImageUrl;
+            video.RelatedToolName = videoModel.RelatedToolName ?? video.RelatedToolName;
+            video.RelatedToolUrl = videoModel.RelatedToolUrl ?? video.RelatedToolUrl;
 
             await SaveModificationAsync(video, modifierId);
         }
 
         public async Task DeleteAsync(string videoId, string modifierId)
         {
-            var video = await this.GetByIdAsync(videoId);
+            var video = await GetByIdAsync(videoId);
 
             await DeleteEntityAsync(video, modifierId);
         }
 
         private async Task ValidateCreateInputAsync(VideoCreateModel videoModel)
         {
-            var isAnyVideo = await this.AnyByStringAsync(videoModel.Url);
+            var isAnyVideo = await AnyByStringAsync(videoModel.Url);
             if (isAnyVideo)
                 throw new ResourceAlreadyExistsException(string.Format(
                     ErrorMessages.EntityAlreadyExists,
                     typeof(Video).Name, videoModel.Url));
         }
 
+        public async Task AssignTagAsync(string videoId, string tagId, string modifierId)
+        {
+            var video = await GetByIdAsync(videoId);
+
+            var isTagAlreadyAssigned = video.Tags.Any(x => x.Id == tagId);
+            if (isTagAlreadyAssigned)
+                throw new ResourceNotFoundException(string.Format(
+                    ErrorMessages.TagAlreadyAssigned, typeof(Tag).Name, tagId));
+
+            var tag = await _tagService.GetTagByIdAsync(tagId);
+            video.Tags.Add(tag);
+
+            await SaveModificationAsync(video, modifierId);
+        }
+
+        public async Task RemoveTag(string videoId, string tagId, string modifierId)
+        {
+            var video = await GetByIdAsync(videoId);
+
+            var isTagAlreadyAssigned = video.Tags.Any(x => x.Id == tagId);
+            if (!isTagAlreadyAssigned)
+                throw new ResourceNotFoundException(string.Format(
+                    ErrorMessages.TagNotAssigned, typeof(Tag).Name, tagId));
+
+            var tag = await _tagService.GetTagByIdAsync(tagId);
+
+            video.Tags.Remove(tag);
+
+            await SaveModificationAsync(video, modifierId);
+        }
+
         public async Task<VideoViewModel> GetVideoViewModelByIdAsync(string id)
         {
-            var isAny = await this.AnyByIdAsync(id);
+            var isAny = await AnyByIdAsync(id);
 
             if (!isAny)
                 throw new ResourceNotFoundException(string.Format(
                     ErrorMessages.EntityDoesNotExist, typeof(Review).Name));
 
-            var videoViewModel = await this.dbContext.Videos
+            var videoViewModel = await dbContext.Videos
                 .AsNoTracking()
                 .Where(x => x.Id == id)
                 .Select(x => new VideoViewModel
@@ -86,7 +122,7 @@
         public async Task<ICollection<VideoPreviewModel>> GetVideoPreviewModelBundleAsync()
         {
 
-            var videoPreviewModelBundle = await this.dbContext.Videos
+            var videoPreviewModelBundle = await dbContext.Videos
                 .AsNoTracking()
                 .Where(x => !x.Deleted)
                 .Select(x => new VideoPreviewModel
@@ -104,13 +140,13 @@
         public async Task<VideoEditViewModel> GetVideoEditViewModelByIdAsync(string id)
         {
 
-            var isAny = await this.AnyByIdAsync(id);
+            var isAny = await AnyByIdAsync(id);
 
             if (!isAny)
                 throw new ResourceNotFoundException(string.Format(
                     ErrorMessages.EntityDoesNotExist, typeof(Review).Name));
 
-            var videoEditViewModel = await this.dbContext.Videos
+            var videoEditViewModel = await dbContext.Videos
             .AsNoTracking()
             .Where(x => x.Id == id && !x.Deleted)
             .Select(x => new VideoEditViewModel
@@ -126,13 +162,13 @@
 
         public async Task<VideoDeleteViewModel> GetVideoDeleteViewModelByIdAsync(string id)
         {
-            var isAny = await this.AnyByIdAsync(id);
+            var isAny = await AnyByIdAsync(id);
 
             if (!isAny)
                 throw new ResourceNotFoundException(string.Format(
                     ErrorMessages.EntityDoesNotExist, typeof(Review).Name));
 
-            var videoDeleteViewModel = await this.dbContext.Videos
+            var videoDeleteViewModel = await dbContext.Videos
             .AsNoTracking()
             .Where(x => x.Id == id && !x.Deleted)
             .Select(x => new VideoDeleteViewModel
