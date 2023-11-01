ab43e127189d5ad442423e78a7500f9930a17e4f
MilenDinev <dinev.milen@gmail.com>
2023-11-01 20:25:34 +0200

    Initial Create

---
+++ Blog.Data.Models/ViewModels/PricingStrategy/PricingStrategyViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Data.Models.ViewModels.PricingStrategy
+{
+    public class PricingStrategyViewModel
+    {
+        public string Id { get; set; }
+        public string Strategy { get; set; }
+    }
+}
---
+++ Blog.Data.Models/ViewModels/Tag/TagViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Data.Models.ViewModels.Tag
+{
+    public class TagViewModel
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
