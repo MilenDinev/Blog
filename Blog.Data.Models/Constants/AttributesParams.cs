c0165164315c8027fa193bd6ddfcd6a2692788fe
MilenDinev <dinev.milen@gmail.com>
2023-11-08 16:12:03 +0200

    Updating validation parameters

--- Blog.Data.Models/Constants/AttributesParams.cs
+++ Blog.Data.Models/Constants/AttributesParams.cs
@@ -2,24 +2,24 @@
 {
     internal static class AttributesParams
     {
-        internal const int TitleMinLength = 5;
-        internal const int TitleMaxLength = 100;
+        internal const int TitleMinLength = 3;
+        internal const int TitleMaxLength = 50;
 
-        internal const int DescriptionMinLength = 20;
-        internal const int DescriptionMaxLength = 200;
+        internal const int DescriptionMinLength = 30;
+        internal const int DescriptionMaxLength = 100;
 
         internal const int ContentMinLength = 200;
 
         internal const int ProviderMinLength = 3;
-        internal const int ProviderMaxLength = 50;
+        internal const int ProviderMaxLength = 30;
 
         internal const int StrategyMinLength = 3;
         internal const int StrategyMaxLength = 20;
 
         internal const int TagValueMinLength = 3;
-        internal const int TagValueMaxLength = 10;
+        internal const int TagValueMaxLength = 15;
 
         internal const int RelatedToolNameMinLength = 3;
-        internal const int RelatedToolNameMaxLength = 30;
+        internal const int RelatedToolNameMaxLength = 50;
     }
 }
