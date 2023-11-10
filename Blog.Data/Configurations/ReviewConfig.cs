81df15fb2f94a752904863977d667ce005dcc145
MilenDinev <dinev.milen@gmail.com>
2023-11-10 02:20:09 +0200

    Removing unused config

--- Blog.Data/Configurations/ReviewConfig.cs
+++ Blog.Data/Configurations/ReviewConfig.cs
@@ -1,30 +1,14 @@
 ﻿namespace Blog.Data.Configurations
 {
-    using Microsoft.EntityFrameworkCore.Metadata.Builders;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
     using Entities;
+    using Microsoft.Data.SqlClient;
 
     internal class ReviewConfig : IEntityTypeConfiguration<Review>
     {
         public void Configure(EntityTypeBuilder<Review> builder)
         {
-
-            builder.HasMany(a => a.Tags)
-                .WithMany(t => t.Reviews)
-                .UsingEntity<Dictionary<string, object>>("ReviewsTags",
-            a => a.HasOne<Tag>().WithMany().HasForeignKey("TagId")
-                .OnDelete(DeleteBehavior.Cascade),
-            t => t.HasOne<Review>().WithMany().HasForeignKey("ReviewId")
-                .OnDelete(DeleteBehavior.Restrict));
-
-
-                builder.HasMany(a => a.FavoriteByUsers)
-                    .WithMany(u => u.FavoriteReviews)
-                    .UsingEntity<UsersFavoriteReviews>(
-                a => a.HasOne<User>().WithMany().HasForeignKey("UserId")
-                    .OnDelete(DeleteBehavior.Restrict),
-                u => u.HasOne<Review>().WithMany().HasForeignKey("ReviewId")
-                    .OnDelete(DeleteBehavior.Restrict));
 
             builder.HasOne(a => a.Creator)
                 .WithMany()
