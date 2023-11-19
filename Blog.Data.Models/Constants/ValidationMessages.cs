29b5c00d95b8a6e97efc39ac60e85b9e9cefc7b0
MilenDinev <dinev.milen@gmail.com>
2023-11-19 21:13:28 +0200

    Optimizing requests encoding

--- Blog.Data.Models/Constants/ValidationMessages.cs
+++ Blog.Data.Models/Constants/ValidationMessages.cs
@@ -3,8 +3,8 @@
     internal static class ValidationMessages
     {
         internal const string Required = "{0} is required!";
-        internal const string MinMaxLength = "{0} lenght must be between {2} and {1} symbols!";
-        internal const string MinLength = "{0} lenght must be atleast {1} symbols!";
+        internal const string MinMaxLength = "{0} length must be between {2} and {1} symbols!";
+        internal const string MinLength = "{0} length must be atleast {1} symbols!";
         internal const string URL = "{0} must be a valid URL addres!";
     }
 }
