8ce5423ba0ada040e425c7db84714e2b9e1b5bb9
MilenDinev <dinev.milen@gmail.com>
2024-10-12 21:30:27 +0300

    Update

---
+++ Blog.Data/Configurations/Shared/ToolsPricingStrategiesConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace Blog.Data.Configurations.Shared
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Entities.Shared;
+
+    internal class ToolsPricingStrategiesConfig : IEntityTypeConfiguration<ToolsPricingStrategies>
+    {
+        public void Configure(EntityTypeBuilder<ToolsPricingStrategies> builder)
+        {
+            builder.HasOne(x => x.Tool)
+                .WithMany(x => x.PricingStrategies)
+                .HasForeignKey(x => x.ToolId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.PricingStrategy)
+                .WithMany(x => x.Tools)
+                .HasForeignKey(x => x.PricingStrategyId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasKey(x => new { x.ToolId, x.PricingStrategyId });
+        }
+    }
+}
---
+++ Blog.Services/AutoMapperProfiles/ToolMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿namespace Blog.Services.AutoMapperProfiles
+{
+    using AutoMapper;
+    using Data.Entities;
+    using Data.ViewModels.Tool;
+    using Data.Models.RequestModels.Tool;
+
+    public class ToolMappingProfile : Profile
+    {
+        public ToolMappingProfile()
+        {
+            CreateMap<ToolCreateModel, Tool>()
+                .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.Title.ToUpper()))
+                .ForMember(e => e.TopPick, m => m.MapFrom(m => m.TopPick))
+                .ForMember(e => e.SpecialOffer, m => m.MapFrom(m => m.SpecialOffer))
+                .ForMember(e => e.Tags, m => m.Ignore());
+            CreateMap<Tool, ToolPreviewModel>()
+                .ForMember(m => m.UpVotes, e => e.MapFrom(e => e.Votes.Count(x => x.Type == true && !x.Deleted)))
+                .ForMember(m => m.CreationDate, e => e.MapFrom(e => e.CreationDate.ToString("dd MMMM hh:mm tt")))
+                .ForMember(m => m.Tags, e => e.MapFrom(e => e.Tags.Select(t => t.Tag.Value).ToList()))
+                .ForMember(m => m.PricingStrategies, e => e.MapFrom(e => e.PricingStrategies.Select(t => t.PricingStrategy.Strategy).ToList()));
+
+        }
+    }
+}
